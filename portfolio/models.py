2f623e03f83c30fe618fc1f4540120f24003caf3
sunshane6726 <sunshane6726@gmail.com>
2019-06-30 18:18:12 +0900

    ReStartheroku3

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Portfolio(models.Model): # class는 항상 대문자로 시작을 해야한다.
-    objects = models.Manager()
+class Portfolio(models.Model): # class는 항상 대문자로 시작을 해야한다. 
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='./images/')
     description = models.CharField(max_length=500)
