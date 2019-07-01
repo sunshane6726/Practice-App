b4a7423a99cec1c1d1b2a0251d14f9c04a611e96
sunshane6726 <sunshane6726@gmail.com>
2019-07-01 22:27:46 +0900

    Error1

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['0.0.0.0','localhost',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,8 +75,12 @@ DATABASES = {
 
 import dj_database_url
 
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+
 
 
 
@@ -130,5 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-
 
